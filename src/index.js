b43398a8269d60b2474a20f1f2a16ba0f53385b6
Anshul Sahni <anshul_sahni@live.com>
2016-11-19 22:38:22 -0800

    implementing mountCSSToDocument in default

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ var combineCSSAndProps = require('./combineCSSAndProps');
 var getGlobalClassName = require('./getGlobalClassName');
 var getClassesFromCSS = require('./getClassesFromCSS');
 var replaceOldClassWithNew = require('./replaceOldClassWithNew');
+var mountCSSToDocument = require('./mountCSSToDocument');
 
 module.exports = function() {
   var css = combineCSSAndProps(_.first(arguments), _.tail(arguments));
@@ -15,7 +16,7 @@ module.exports = function() {
 
     var cssClassRegExp = new RegExp(cssClass + '\\s');
     css = replaceOldClassWithNew(css, cssClassRegExp, globalClass);
-
+    mountCSSToDocument(css);
     classMap[cssClass] = globalClass;
   });
 
