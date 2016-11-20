aaac0bbeaefacfdc84efb317c5bd97061dd102ed
Anshul Sahni <anshul_sahni@live.com>
2016-11-19 18:04:58 -0800

    [bug-fixed] flattening the array before returing from getClassesFromCSS

--- src/getClassesFromCSS.js
+++ src/getClassesFromCSS.js
@@ -3,7 +3,7 @@ var _ = require('lodash');
 const CLASS_PATTERN = /\.\w+\s?\{/g;
 
 module.exports = function(css) {
-  return _.map(css.match(CLASS_PATTERN), function(patternMatch) {
+  return _.flatten(_.map(css.match(CLASS_PATTERN), function(patternMatch) {
     return _.words(_.join(_.tail(patternMatch), ''));
-  });
+  }));
 }
