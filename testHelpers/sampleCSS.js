e32a92c682ad232cdbddb9dcdd032e42d2872978
Anshul Sahni <anshul_sahni@live.com>
2016-11-19 18:19:46 -0800

    added sampleCSS as testHelpers

---
+++ testHelpers/sampleCSS.js
@@ -0,0 +1,12 @@
+var sampleCSS = '@media (min-width: 600px) {'
++ '  .notifications {'
++ '    padding: 15px;'
++ '  }'
++ '}'
++ '.notification {'
++ '  border: 10px solid red;'
++ '  padding: 10px;'
++ '  color: #333;'
++ '}';
+
+export default sampleCSS;
