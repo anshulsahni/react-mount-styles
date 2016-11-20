10fe9942588e20091b5253bbcbd210fe42ea9b7c
Anshul Sahni <anshul_sahni@live.com>
2016-11-19 17:32:45 -0800

    if className is null getGlobalClassName returns null

--- src/getGlobalClassName.js
+++ src/getGlobalClassName.js
@@ -3,5 +3,5 @@ var _ = require('lodash');
 const GLOBAL_CLASS_PREFIX = '--react-mount-styles__';
 
 module.exports = function(className) {
-  return className + _.uniqueId(GLOBAL_CLASS_PREFIX);
+  return className ? className + _.uniqueId(GLOBAL_CLASS_PREFIX) : null;
 }
