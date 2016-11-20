9bd3b06ac02c2a2ea35e2f1f6f8fc6ade95d1ed7
Anshul Sahni <anshul_sahni@live.com>
2016-11-19 17:36:03 -0800

    added test for checking null values from getGlobalClassName

--- test/getGlobalClassName.js
+++ test/getGlobalClassName.js
@@ -2,7 +2,16 @@ var assert = require('assert');
 var getGlobalClassName = require('../src/getGlobalClassName');
 
 describe('getGlobalClassName', function() {
+
   it('should return global class name', function() {
     assert.equal('test--react-mount-styles__1', getGlobalClassName('test'));
+    assert.equal('test--react-mount-styles__2', getGlobalClassName('test'));
+    assert.equal('ansh--react-mount-styles__3', getGlobalClassName('ansh'));
+  });
+
+  it('should return null for empty or null value being passed', function() {
+    assert.equal(null, getGlobalClassName(null));
+    assert.equal(null, getGlobalClassName(undefined));
+    assert.equal(null, getGlobalClassName(''));
   });
 });
