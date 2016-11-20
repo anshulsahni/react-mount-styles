ddc5d31371833e381c707ebe020d16c94864326b
Anshul Sahni <anshul_sahni@live.com>
2016-11-19 22:19:33 -0800

    added support for class names having hyphen

--- src/getClassesFromCSS.js
+++ src/getClassesFromCSS.js
@@ -1,6 +1,6 @@
 var _ = require('lodash');
 
-const CLASS_PATTERN = /\.\w+\s?\{/g;
+const CLASS_PATTERN = /\.(\w|-)+\s?\{/g;
 
 module.exports = function(css) {
   return _.flatten(_.map(css.match(CLASS_PATTERN), function(patternMatch) {
